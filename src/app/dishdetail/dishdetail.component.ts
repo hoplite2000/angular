4e8e73fd9cf83a533ace73d4df1ba6ea904c9143
nehal2000 <nehalnshet05@gmail.com>
2020-06-16 18:58:07 +0530

    animations1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,11 +5,26 @@ import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Comment } from '../shared/comment';
+import { trigger, style, state, animate, transition } from '@angular/animations';
+import { setClassMetadata } from '@angular/core/src/r3_symbols';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1 
+      })), 
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -20,6 +35,7 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next: string;
   dishIDs: string[];
+  visibility = 'shown';
   commentErrors = {
     'author':'',
     'comment':''
@@ -47,8 +63,8 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getdishIDs()
       .subscribe((dishids) => this.dishIDs = dishids);
 
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe((dish) => {this.dish = dish; this.dishcopy = dish; this.nextprev(dish.id);},
+    this.route.params.pipe(switchMap((params: Params) => {this.visibility = 'hidden'; return this.dishservice.getDish(params['id']);}))
+      .subscribe((dish) => {this.dish = dish; this.dishcopy = dish; this.nextprev(dish.id); this.visibility = 'shown;'},
         errmsg => this.errmsg = <any>errmsg); 
   }
 
